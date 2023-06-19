8d1acfbe7ccc16fffcbed48ca0afd2f08a0beb2d
sofilin321 <sofigalleano25@gmail.com>
2023-06-19 00:11:22 -0300

    vamosdenuevo

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-function aPagar (cantidad,categoria){
-let estudiante = 40;
-let trainee = 100;
-let junior = 170;
-let resultado = 0;
-
-if (categoria == "estudiante"){
-    resultado = cantidad * estudiante;
-} else if (categoria == "trainee"){
- resultado = cantidad * trainee;
-} else {
- resultado = cantidad * junior;
-}
-
-return resultado;
-}
-
-let formulario = document.getElementsByTagName("form");
-let categoria = document.getElementsByClassName("categoria");
-let cantidad = document.getElementsByClassName("cantidad");
-let total = document.getElementsByClassName("total");
-
-
+function descuento(cantidad,categoria) {
+    let estudiante = 0.2
+    let trainee = 0.5
+    let junior = 0.85
+    let resultado = 0
   
-formulario.addEventListener("submit", (evento)=>{
-    evento.preventDefault();
-    total.textContent = "Total a pagar: $" + aPagar(cantidad.value,categoria.value);
+    if (categoria == "estudiante"){
+        resultado = cantidad * 200 * estudiante
+    }else if (categoria == "trainee") {
+         resultado = cantidad * 200 * trainee
+    } else {
+        resultado = cantidad * 200 * junior
+    }
+    return resultado
+  }
+  
+  let formulario = document.getElementById("form_compra")
+  
+  let categorias = document.getElementById("categoria")
+  
+  let cantidades = document.getElementById("cantidad")
+  
+  let divTotal = document.getElementById("total")
+  
+  formulario.addEventListener("reset", ()=>{
+    divTotal.textContent = "Total a pagar: "
   })
-
+  
+  formulario.addEventListener("submit", (evento)=>{
+    evento.preventDefault();
+    divTotal.textContent = "Total a pagar: $" + descuento(cantidades.value,categorias.value)
+  })
 
 
 
